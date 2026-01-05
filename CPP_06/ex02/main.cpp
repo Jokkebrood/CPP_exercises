0394e84bc90980b6d9ce8f65c17fa856f91de0f4
Jacobus De Vlamynck <jade-vla@shi-r8-p2.s19.be>
2026-01-05 15:14:07 +0100

    Added identify functions in main (CPP06)

--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -13,17 +13,44 @@ Base* generate()
 
 	else if (t->tm_sec % 3 == 1)
 		base = new B();
-	
+
 	else
 		base = new C();
 
 	return base;
 }
 
+void identify(Base *p)
+{
+	//std::cout << "Pointer identify called" << std::endl;
+	if (dynamic_cast<A const*>(p))
+		std::cout  << "A" << std::endl;
+
+	else if (dynamic_cast<B const*>(p))
+		std::cout << "B" <<std::endl;
+
+	else
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base &p)
+{
+	//std::cout << "Reference identify called" << std::endl;
+	if (dynamic_cast<A const*>(&p))
+		std::cout  << "A" << std::endl;
+
+	else if (dynamic_cast<B const*>(&p))
+		std::cout << "B" <<std::endl;
+
+	else
+		std::cout << "C" << std::endl;
+}
+
 int main()
 {
 	Base *test = generate();
-	std::cout << *test << std::endl;
+	identify(*test);
+	identify(test);
 	delete test;
 	return 0;
 }
